e0a537a047a099b192a9b9a6ff0768a7b9d4cab7
Chen-Huayi <1035766902@qq.com>
2022-06-07 00:24:59 -0500

    update

--- repositories.js
+++ repositories.js
@@ -1,25 +1,25 @@
 document.getElementById('data-mining').innerHTML = marked.parse(
-    '[![](https://github-readme-stats.vercel.app/api/pin/?username=chen-huayi&repo=data_mining&show_owner=true)](https://github.com/Chen-Huayi/Data_Mining)'
+    '[![](https://github-readme-stats.vercel.app/api/pin/?username=chen-huayi&repo=data_mining)](https://github.com/Chen-Huayi/Data_Mining)'
 );
 
 document.getElementById('android-aas').innerHTML = marked.parse(
-    '[![](https://github-readme-stats.vercel.app/api/pin/?username=chen-huayi&repo=Android_app&show_owner=true)](https://github.com/Chen-Huayi/Android_app)'
+    '[![](https://github-readme-stats.vercel.app/api/pin/?username=chen-huayi&repo=Android_app)](https://github.com/Chen-Huayi/Android_app)'
 );
 
 document.getElementById('whodunit').innerHTML = marked.parse(
-    '[![](https://github-readme-stats.vercel.app/api/pin/?username=chen-huayi&repo=Whodunit&show_owner=true)](https://github.com/Chen-Huayi/Whodunit)'
+    '[![](https://github-readme-stats.vercel.app/api/pin/?username=chen-huayi&repo=Whodunit)](https://github.com/Chen-Huayi/Whodunit)'
 );
 
 document.getElementById('game-of-life').innerHTML = marked.parse(
-    '[![](https://github-readme-stats.vercel.app/api/pin/?username=chen-huayi&repo=Game_of_Life&show_owner=true)](https://github.com/Chen-Huayi/Game_of_Life)'
+    '[![](https://github-readme-stats.vercel.app/api/pin/?username=chen-huayi&repo=Game_of_Life)](https://github.com/Chen-Huayi/Game_of_Life)'
 );
 
 document.getElementById('my-web').innerHTML = marked.parse(
-    '[![](https://github-readme-stats.vercel.app/api/pin/?username=chen-huayi&repo=myWeb&show_owner=true)](https://github.com/Chen-Huayi/myWeb)'
+    '[![](https://github-readme-stats.vercel.app/api/pin/?username=chen-huayi&repo=myWeb)](https://github.com/Chen-Huayi/myWeb)'
 );
 
 document.getElementById('huffman-encode').innerHTML = marked.parse(
-    '[![](https://github-readme-stats.vercel.app/api/pin/?username=chen-huayi&repo=Huffman-Encoding&show_owner=true)](https://github.com/Chen-Huayi/Huffman-Encoding)'
+    '[![](https://github-readme-stats.vercel.app/api/pin/?username=chen-huayi&repo=Huffman-Encoding)](https://github.com/Chen-Huayi/Huffman-Encoding)'
 );
 
 
